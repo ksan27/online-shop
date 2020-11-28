e5892f75d2d62ffc4643e4584a8fd465e0ccdcc5
ksan27 <k.saisai-girl@ezweb.ne.jp>
2020-11-28 16:24:18 +0900

    トップページの作成

--- db/migrate/20201128071052_create_shops.rb
+++ db/migrate/20201128071052_create_shops.rb
@@ -1,8 +1,9 @@
 class CreateShops < ActiveRecord::Migration[6.0]
   def change
     create_table :shops do |t|
-      t.string :name
-      t.text :shop_url
+      t.string :name,   null: false
+      t.text :shop_url, null: false
+      t.references :user, null: false, foreign_key: true
       t.timestamps
     end
   end
