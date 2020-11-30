b099554a4e982818aa172cfec5ea6022145b5109
ksan27 <k.saisai-girl@ezweb.ne.jp>
2020-11-30 22:44:29 +0900

    テストコードの実装

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,7 +5,50 @@ RSpec.describe User, type: :model do
   end
 
   describe 'ユーザー新規登録' do
-    it '必須項目がすべて存在すれば保存できる' do
+    context '新規登録がうまくいくとき' do
+      it '必須項目がすべて存在すれば保存できる' do
+        expect(@user).to be_valid
+      end
+      it 'パスワードが6文字以上であれば保存できる' do
+        @user.password = "000000"
+        expect(@user).to be_valid
+      end
+    end
+
+    context '新規登録がうまくいかないとき' do
+      it 'ニックネームが空だと保存できない' do
+        @user.nickname = ""
+        @user.valid?
+        expect(@user.errors.full_messages).to include("ニックネームを入力してください")
+      end
+      it 'Eメールが空だと保存できない' do
+        @user.email = ""
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Eメールを入力してください")
+      end
+      it 'Eメールに@がないと保存できない' do
+      end
+      it 'パスワードが空だと保存できない' do
+        @user.password = ""
+        @user.valid?
+        expect(@user.errors.full_messages).to include("パスワードを入力してください")
+      end
+      it 'パスワードが5文字以下だと保存できない' do
+        @user.password = "00000"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("パスワードは6文字以上で入力してください")
+      end
+      it "パスワードと確認用パスワードが不一致だと保存できない" do
+        @user.password = "111111"
+        @user.password_confirmation = "000000"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("パスワード（確認用）とパスワードの入力が一致しません")
+      end
+      it "パスワードが存在していても確認用パスワードが空だと保存できない" do
+        @user.password_confirmation = ""
+        @user.valid?
+        expect(@user.errors.full_messages).to include("パスワード（確認用）とパスワードの入力が一致しません")
+      end
     end
   end
 end
