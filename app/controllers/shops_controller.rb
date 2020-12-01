0c36d4b89f0c29c8105ca7874612ffa024218bd1
ksan27 <k.saisai-girl@ezweb.ne.jp>
2020-12-01 17:18:44 +0900

    エラーメッセージの編集

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -8,5 +8,16 @@ class ShopsController < ApplicationController
   end
 
   def create
+    @shop = Shop.create(shop_params)
+    if @shop.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def shop_params
+    params.require(:shop).permit(:name, :shop_url, :image).merge(user_id: current_user.id)
   end
 end
