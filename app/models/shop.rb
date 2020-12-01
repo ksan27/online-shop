60849ae915a288af003eb2b3b0202231648b4562
ksan27 <k.saisai-girl@ezweb.ne.jp>
2020-12-01 19:06:20 +0900

    単体テストコード(shop)

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -4,7 +4,7 @@ class Shop < ApplicationRecord
   
   with_options presence: true do
     validates :image
-    validates :name, format: {with: /\A[ぁ-んァ-ン一-龥][a-zA-Z0-9]+\z/, message:"は登録出来ない値です"}
+    validates :name, format: {with: /\A[ぁ-んァ-ン一-龥a-zA-Z0-9ａ-ｚ０-９Ａ-Ｚ]+\z/, message:"は登録出来ない値です"}
     validates :shop_url, format: {with: /http(s)?:\/\/([-\w]+\.)+[-\w]+(\/[-\w.\/?%&=]*)?/, message:"は登録出来ない値です"}
   end
 end
