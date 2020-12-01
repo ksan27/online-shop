0c937a7a12f3877e18ec5381a77f8500197f33d9
ksan27 <k.saisai-girl@ezweb.ne.jp>
2020-12-01 12:04:37 +0900

    投稿機能のビュー作成

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -2,4 +2,11 @@ class ShopsController < ApplicationController
   def index
     @shops = Shop.all
   end
+
+  def new
+    @shop = Shop.new
+  end
+
+  def create
+  end
 end
--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,2 +1,5 @@
 class Shop < ApplicationRecord
+  with_options presence: true do
+    validates :name, :shop_url
+  end
 end
