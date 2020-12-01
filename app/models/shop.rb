3559e41a3bf8f487cfe76917699b2af2d1d2340d
ksan27 <k.saisai-girl@ezweb.ne.jp>
2020-12-01 17:19:05 +0900

    バリデーション追加

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,5 +1,10 @@
 class Shop < ApplicationRecord
+  belongs_to :user
+  has_one_attached :image
+  
   with_options presence: true do
-    validates :name, :shop_url
+    validates :image
+    validates :name, format: {with: /\A[ぁ-んァ-ン一-龥][a-zA-Z0-9]+\z/, message:"は登録出来ない値です"}
+    validates :shop_url, format: {with: /http(s)?:\/\/([-\w]+\.)+[-\w]+(\/[-\w.\/?%&=]*)?/, message:"は登録出来ない値です"}
   end
 end
