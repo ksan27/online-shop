ede1dc4aabfc8220a1a6d46f7a75de2acfaa2739
ksan27 <k.saisai-girl@ezweb.ne.jp>
2020-12-08 19:30:26 +0900

    ユーザー編集

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,9 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!, only: [:show]
+
+  def show
+    @users = User.find(params[:id])
+    @user = current_user
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "shops#index"
   resources :shops
+  resources :users, only: [:show]
 end
