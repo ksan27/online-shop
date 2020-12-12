e63de410c84d0ac1d276b79c216fa55ad54acd96
ksan27 <k.saisai-girl@ezweb.ne.jp>
2020-12-12 20:49:16 +0900

    rubocopの実行

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -9,6 +9,6 @@ class Shop < ApplicationRecord
     validates :image
     validates :name, format: { with: /\A[ぁ-んァ-ン一-龥a-zA-Z0-9ａ-ｚ０-９Ａ-Ｚ]+\z/, message: 'は登録出来ない値です' }
     validates :shop_url, format: { with: %r{http(s)?://([-\w]+\.)+[-\w]+(/[-\w./?%&=]*)?}, message: 'は登録出来ない値です' }
-    validates :genre_id, numericality: { other_than: 1 } 
+    validates :genre_id, numericality: { other_than: 1 }
   end
 end
